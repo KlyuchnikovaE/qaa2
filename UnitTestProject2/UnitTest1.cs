7d8300e1d09806bbdd118e056aded8092bf4cff1
klyuchnikovaE <klyuchnukova_enemy@list.ru>
2017-10-24 22:59:40 +0400

    first commit

---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Опрделяем драйвер чтобы работать именно с гугл хром
+            IWebDriver driver = new ChromeDriver();
+            //Переходим на гугл.ру
+            driver.Navigate().GoToUrl("https://www.google.ru");
+            //Ищем элемент ввода информации на сайте гугл по айди
+            IWebElement findEdit = driver.FindElement(By.Id("lst-ib"));
+            //Кликает по найденном элементу
+            findEdit.Click();
+            //И вводим значение в этот элемент
+            findEdit.SendKeys("Симбирсофт");
+        }
+    }
+}
